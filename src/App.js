0b1d77153bb2b4b887b9b092690ab70d93098890
Murhaf-Mo <99115205+Murhaf-Mo@users.noreply.github.com>
2023-09-09 10:14:30 +0300

    Cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import {Cards} from "./components/cards/cards";
+const { Header, Content, Footer } = Layout;
 
-function App() {
+const App = () => {
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout className="layout">
+        <Header
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <div className="demo-logo" />
+          <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              items={new Array(15).fill(null).map((_, index) => {
+                const key = index + 1;
+                return {
+                  key,
+                  label: `nav ${key}`,
+                };
+              })}
+          />
+        </Header>
+        <Content
+            style={{
+              padding: '0 50px',
+            }}
+        >
+          <Breadcrumb
+              style={{
+                margin: '16px 0',
+              }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            {/*<Breadcrumb.Item>List</Breadcrumb.Item>*/}
+            {/*<Breadcrumb.Item>App</Breadcrumb.Item>*/}
+          </Breadcrumb>
+          <div
+              className="site-layout-content"
+              style={{
+                height: '100vh',
+                background: colorBgContainer,
+                padding: 24,
+              }}
+          >
+            Content
 
+          </div>
+        </Content>
+        <Footer
+            style={{
+              textAlign: 'center',
+            }}
+        >
+          DevMinds ©2023 Created by DevMinds
+
+        </Footer>
+      </Layout>
+  );
+};
 export default App;
